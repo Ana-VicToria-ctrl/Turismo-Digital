d1bca0978e327f9eadb71476d2592c56c4db6b7c
Ana-VicToria-ctrl <anavitorianascimento714@gmail.com>
2026-04-09 09:58:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -555,8 +555,6 @@ def delete_attraction(attraction_id):
 init_db()
 
 if __name__ == "__main__":
- import os
-
-if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
